7227d97676875e055dbb7845d853c0fb82b7610e
DSK <162319484+Satwik2529@users.noreply.github.com>
2026-06-27 11:20:27 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,20 @@
+class KthLargest {
+    priority_queue<int,vector<int> ,greater<int>> pq;
+    int capacity;
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+        capacity=k;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        while(pq.size()>capacity)pq.pop();
+        return pq.top();
+        
+    }
+};
